667e068446493f167b0b93ca26c14ab495ca75c5
Durgesh Agrawal <durgesha@iitk.ac.in>
2020-05-22 10:44:52 +0530

    WIP

--- ZygoteClient.py
+++ ZygoteClient.py
@@ -12,10 +12,14 @@ port = 12345
 
 startTime = time.time()
 
+requestNum = 0
+
 while True:
     sock = socket.socket()
     sock.connect(('127.0.0.1', port))
 
+    sock.send("Request Number "+str(requestNum))
+    requestNum += 1
     print("Client LOG: Message received - "+sock.recv(1024)+" from server at time "+str(time.time()-startTime))
 
     sock.close()
--- ZygoteServer.py
+++ ZygoteServer.py
@@ -7,9 +7,15 @@
 
 import socket
 import time
+import os
 
 usapPoolSizeMax = 10
 usapPoolSizeMin = 5
+
+socketPIDs = []
+pipes = []      # [(readFD, writeFD)]
+numUsaps = 0
+availableIndices = []
 
 sock = socket.socket()
 port = 12345
@@ -22,9 +28,38 @@ print("Listening...")
 
 startTime = time.time()
 
-while True:
-    client, addr = sock.accept()
+newFork = None
+while numUsaps < usapPoolSizeMax:
+    newFork = os.fork()
+
+    if newFork > 0:     # Parent
+        readFD, writeFD = os.pipe()
+        pipes.append((readFD, writeFD))
+        socketPIDs.append(newFork)
+        numUsaps += 1
+        availableIndices += [numUsaps-1]        # As of now, all USAPs are available
+    else:               # Child, don't fork from here
+        break
+
+if newFork > 0:
+    while True:
+        client, addr = sock.accept()
+        indexAcquired = availableIndices.pop()
+        numUsaps -= 1
+        writeFD = pipes[indexAcquired][1]
+        print("Assigning request to PID "+str(socketPIDs[indexAcquired]))
+
+        
+
+
+else:
+
+    client = ##
+
+    clientData = client.recv(1024)
+        
     print("Server LOG: Connection received from "+str(addr)+" at time "+str(time.time()-startTime))
+    print("Data received from client: "+clientData)
 
     client.send("Connection established")
 
