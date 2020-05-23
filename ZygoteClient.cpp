2da40d3ba5ad04ba1a1078817b23a9fe2c44d709
Durgesh Agrawal <durgesha@iitk.ac.in>
2020-05-23 12:43:50 +0530

    Working prototype!

--- ZygoteClient.cpp
+++ ZygoteClient.cpp
@@ -3,41 +3,65 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
+#include <string>
 #define PORT 8080
+
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
 
-    char *data = "Command from client";
-    char buffer[1024] = {0};
+    int reqPerSec = 1;
+    float sleepInterval = 1.0 / reqPerSec;
+    time_t startTime = time(NULL);
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    int requestNum = 0;
+
+    while (true)
     {
-        printf("\n Socket creation error \n");
-        return -1;
+
+        if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+            printf("\n Socket creation error \n");
+            return -1;
+        }
+
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(PORT);
+
+        // Convert IPv4 and IPv6 addresses from text to binary form
+        if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+        {
+            printf("\nInvalid address/ Address not supported \n");
+            return -1;
+        }
+
+        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        {
+            printf("\nConnection Failed \n");
+            return -1;
+        }
+        printf("Success\n");
+        string data = "Command from client: ";
+        data.append(to_string(requestNum));
+
+        char toSend[data.length() + 1];
+        strcpy(toSend, data.c_str());
+
+        char buffer[1024] = {0};
+        requestNum += 1;
+
+        send(sock, toSend, strlen(toSend), 0);
+        printf("Client LOG: Data sent from client.\n");
+        valread = read(sock, buffer, 1024);
+        printf("Client LOG: %s\n", buffer);
+
+        close(sock);
+        usleep(sleepInterval * 1e6);
     }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-
-    send(sock, data, strlen(data), 0);
-    printf("Data sent from client.\n");
-    valread = read(sock, buffer, 1024);
-    printf("%s\n", buffer);
     return 0;
 }
--- ZygoteServer.cpp
+++ ZygoteServer.cpp
@@ -4,16 +4,69 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <time.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <signal.h>
+#include <sys/wait.h>
+
 #define PORT 8080
+
+// USE SIGNALS!!!!!!!!!!!!!!!!!!!!
+
+using namespace std;
+
+struct sockaddr_in address;
+int addrlen = sizeof(address);
+int server_fd;
+int parentPID;
+int activeUsaps = 0;
+
+void sigint(int snum);
+void sigusr1(int snum);
+
+void refillUsaps(int &numUsaps, int usapPoolSizeMax, vector<int> &socketPIDs, vector<int> &availableIndices, vector<int> &unavailableIndices)
+{
+    // printf("FFFFFFF %d\n", getpid());
+    while (numUsaps < usapPoolSizeMax)
+    {
+        int pid = fork();
+        if (pid == 0)
+            return;
+        else
+        {
+            int unavailableIndex = unavailableIndices.back();
+            unavailableIndices.pop_back();
+            socketPIDs[unavailableIndex] = pid;
+            numUsaps += 1;
+            availableIndices.push_back(unavailableIndex);
+        }
+    }
+    return;
+}
 
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
+    void childTerminated(int);
+    signal(SIGCHLD, childTerminated);
+
     int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *data = "Response from server";
+
+    int usapPoolSizeMax = 1;
+    int usapPoolSizeMin = 1;
+    vector<int> socketPIDs;
+    int numUsaps = 0;
+    vector<int> availableIndices;
+    vector<int> unavailableIndices;
+
+    parentPID = getpid();
+
+    printf("Parent PID: %d\n", parentPID);
+
+    // Registering sighandlers
+    signal(SIGINT, sigint);   // parent gives a green signal to child
+    signal(SIGUSR1, sigusr1); // child sends message to parent that a connection request has been received
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -48,15 +101,120 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                             (socklen_t *)&addrlen)) < 0)
+    int pid = -1;
+
+    // Forking...
+    printf("Forking...\n");
+
+    while (numUsaps < usapPoolSizeMax)
     {
+        pid = fork();
+        if (pid == 0)
+            break; // Child
+        else
+        {
+            // std::cout << "OOLALA" << pid << std::endl;
+            socketPIDs.push_back(pid);
+            availableIndices.push_back(numUsaps);
+            numUsaps += 1;
+        }
+    }
+
+    if (getpid() == parentPID)
+    {
+        while (true)
+        {
+            // // Refill if no USAP available
+            // if (availableIndices.size() == 0)
+            // {
+            //     refillUsaps(numUsaps, usapPoolSizeMax, socketPIDs, availableIndices, unavailableIndices);
+            // }
+
+            while (activeUsaps >= usapPoolSizeMax)
+            {
+                usleep(1e4);
+                continue;
+            }
+
+            int indexAcquired = availableIndices.back();
+            availableIndices.pop_back();
+            unavailableIndices.push_back(indexAcquired);
+            numUsaps -= 1;
+
+            printf("Assigning next request to PID: %d\n", socketPIDs[indexAcquired]);
+
+            kill(socketPIDs[indexAcquired], SIGINT);
+
+            activeUsaps += 1;
+            // printf("ACTIVE USAPS: %d\n", activeUsaps);
+
+            if (numUsaps < usapPoolSizeMin)
+            {
+                refillUsaps(numUsaps, usapPoolSizeMax, socketPIDs, availableIndices, unavailableIndices);
+                if (getpid() != parentPID)
+                    break;
+            }
+            // printf("AAA %d %d\n", numUsaps, getpid());
+
+            // pause(); // Receives SIGUSR1, saying that child has received a connection request. Can proceed with assigning next request to a new PID
+
+            // // Don't create any more process until actveUsaps is maxed out
+            // while (activeUsaps >=     usapPoolSizeMax)
+            // {
+            //     usleep(1e4);
+            //     continue;
+            // }
+        }
+    }
+    if (getpid() != parentPID)
+    {
+        std::cout << "Child process: " << getpid() << std::endl;
+        pause();
+    }
+
+    return 0;
+}
+
+void sigint(int snum)
+{
+
+    signal(SIGINT, sigint);
+    int client;
+    char *data = "Response from server";
+    int valread;
+    char buffer[1024] = {0};
+    printf("SIGINT Received %d\n", getpid());
+
+    if ((client = accept(server_fd, (struct sockaddr *)&address,
+                         (socklen_t *)&addrlen)) < 0)
+    {
+        printf("Failure!\n");
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
-    send(new_socket, data, strlen(data), 0);
+    printf("Connection accepted!\n");
+    kill(parentPID, SIGUSR1);
+
+    valread = read(client, buffer, 1024);
+    printf("Server LOG %d: Data read from client.\n", getpid());
+    printf("Server LOG %d: %s\n", getpid(), buffer);
+    send(client, data, strlen(data), 0);
     printf("Response sent from server\n");
-    return 0;
+    close(client);
+    exit(0);
+}
+
+void sigusr1(int snum)
+{
+    return;
+}
+
+void childTerminated(int snum)
+{
+    int pid;
+    int status;
+
+    pid = wait(&status);
+    activeUsaps -= 1;
+    // printf("");
 }
